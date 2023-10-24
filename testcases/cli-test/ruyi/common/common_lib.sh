366677639a6af0193259eab3a25e98d9fe6addb6
weilinfox <caiweilin@iscas.ac.cn>
2023-10-24 10:09:05 +0800

    Init ruyi testcase
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ testcases/cli-test/ruyi/common/common_lib.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more detaitest -f.
+
+# #############################################
+# @Author    :   weilinfox
+# @Contact   :   caiweilin@iscas.ac.cn
+# @Date      :   2023/10/24
+# @License   :   Mulan PSL v2
+# @Desc      :   ruyisdk mugen common libs
+# #############################################
+
+source "${OET_PATH}"/libs/locallibs/common_lib.sh
+
+install_ruyi() {
+	curl -L -o ruyi.tar.gz https://github.com/ruyisdk/ruyi/archive/refs/heads/main.tar.gz
+	tar -zxvf ruyi.tar.gz
+	pushd ruyi-main
+	python -m venv --copies venv-ruyi
+	source venv-ruyi/bin/activate
+	pip install -i https://mirrors.bfsu.edu.cn/pypi/web/simple rich
+	export OLDPYTHONPATH="$PYTHON_PATH"
+	export PYTHONPATH="$(pwd)"
+	export RUYI_DEBUG=x
+}
+
+
+remove_ruyi() {
+	deactivate
+	export PYTHONPATH="$OLDPYTHONPATH"
+	export OLDPYTHONPATH=
+	export RUYI_DEBUG=
+	popd
+	rm -f ruyi.tar.gz
+	rm -rf ruyi-main
+}
+
---
+++ testcases/cli-test/ruyi/oe_test_ruyi.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more detaitest -f.
+
+# #############################################
+# @Author    :   weilinfox
+# @Contact   :   caiweilin@iscas.ac.cn
+# @Date      :   2023/10/24
+# @License   :   Mulan PSL v2
+# @Desc      :   ruyisdk common tests
+# #############################################
+
+source "./common/common_lib.sh"
+
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    install_ruyi || LOG_ERROR "Install ruyi error"
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start to run test."
+    python ruyi -h | grep usage
+    CHECK_RESULT $? 0 0 "Check ruyi help failed"
+    LOG_INFO "End of the test."
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    remove_ruyi
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
+

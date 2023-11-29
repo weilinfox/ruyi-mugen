7621c0effe644dd796b5f5f02e9af7cf2d123d6b
weilinfox <caiweilin@iscas.ac.cn>
2023-11-29 19:21:06 +0800

    Use --simulate instead of --assume-no
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- libs/locallibs/deb_manage.py
+++ libs/locallibs/deb_manage.py
@@ -76,7 +76,7 @@ def deb_install(pkgs, node=1, tmpfile=""):
         mugen_log.logging("info", "unsupported package manager: apt-get")
         return 0, None
 
-    repoCode, result = func(conn=conn, cmd="apt-get --assume-no --no-show-upgraded --no-install-recommends install " +
+    repoCode, result = func(conn=conn, cmd="apt-get --simulate --no-show-upgraded --no-install-recommends install " +
                                            pkgs)
     # repoCode: 0 already the newest version, 1 something can be done but N, 100 no such package
     if repoCode not in [0, 1]:
@@ -94,7 +94,7 @@ def deb_install(pkgs, node=1, tmpfile=""):
 
     depCode, depList = func(
         conn=conn,
-        cmd="apt-get --assume-no --no-show-upgraded --no-install-recommends install "
+        cmd="apt-get --simulate --no-show-upgraded --no-install-recommends install "
         + pkgs
         + ' 2>&1 | grep -iA 1000 "NEW packages will be installed" | grep -E "^  "'
     )

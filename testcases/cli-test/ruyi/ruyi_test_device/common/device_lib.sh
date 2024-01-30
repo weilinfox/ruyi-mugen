b845e1fbab16560e0fec64d9ad7c12c9dcb3f6e3
weilinfox <caiweilin@iscas.ac.cn>
2024-01-30 13:32:34 +0800

    Fix device test
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- testcases/cli-test/ruyi/ruyi_test_device/common/device_lib.sh
+++ testcases/cli-test/ruyi/ruyi_test_device/common/device_lib.sh
@@ -10,6 +10,7 @@
 
 # #############################################
 # @Author    :   KotorinMinami
+# @Author    :   weilinfox
 # @Contact   :   huangshuo4@gmail.com
 # @Date      :   2024/1/29
 # @License   :   Mulan PSL v2
@@ -33,28 +34,44 @@ function test_ouput() {
 
 function recursion_run() {
     local now_exec=$1
+    local end_exec=$2
     
-    echo -e $now_exec | ruyi device provision | grep -Ev '^$|#' | tail -5 > /tmp/ruyi_device/output
+    if [[ ! -z "$end_exec" && "$end_exec" != "0" ]]; then
+        echo -e $now_exec | ruyi device provision > /tmp/ruyi_device/output
+	echo "Happy hacking! $(expr $end_exec - 1) $?" >> /tmp/ruyi_device/output
+    else
+        echo -e $now_exec | ruyi device provision | grep -Ev '^$|#' | tail -5 > /tmp/ruyi_device/output
+    fi
 
     grep 'Happy hacking!' /tmp/ruyi_device/output
     if [[ $? -eq 0 ]]; then
-        mv /tmp/ruyi_device/output /tmp/ruyi_device/output_${now_exec}
+	tail -20 /tmp/ruyi_device/output > /tmp/ruyi_device/output_${now_exec}
+        rm /tmp/ruyi_device/output
         return 0;
     fi
-    grep 'Proceed' /tmp/ruyi_device/output
+    grep "Proceed with flashing" /tmp/ruyi_device/output
     if [[ $? -eq 0 ]]; then
-        test_ouput /tmp/ruyi_device/output 'Proceed'
-        local next_step=(${result_item[@]})
-        for step in ${next_step[@]}; do
-            recursion_run "$now_exec\n$step"
-        done
+        rm -rf /tmp/ruyi_device/test
+        touch /tmp/ruyi_device/test
+        recursion_run "$now_exec\nn" 2
+        recursion_run "$now_exec\ny" 2
         return 0;
     fi
     grep "Please give the path for the target's whole disk" /tmp/ruyi_device/output
     if [[ $? -eq 0 ]]; then
         rm -rf /tmp/ruyi_device/test
         touch /tmp/ruyi_device/test
-        recursion_run "$now_exec\n/tmp/ruyi_device/test"
+        recursion_run "$now_exec\n/tmp/ruyi_device/test" 1
+        return 0;
+    fi
+    grep 'Proceed?' /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        test_ouput /tmp/ruyi_device/output 'Proceed?'
+        local next_step=(${result_item[@]})
+        for step in ${next_step[@]}; do
+            [ $step != 'n' ]
+            recursion_run "$now_exec\n$step" $?
+        done
         return 0;
     fi
     grep 'Choice' /tmp/ruyi_device/output
@@ -62,7 +79,8 @@ function recursion_run() {
         test_ouput /tmp/ruyi_device/output 'Choice'
         local next_step=(${result_item[@]})
         for step in ${next_step[@]}; do
-            recursion_run "$now_exec\n$step"
+            [ $step != 'n' ]
+            recursion_run "$now_exec\n$step" $?
         done
         return 0;
     fi
@@ -71,22 +89,28 @@ function recursion_run() {
         test_ouput /tmp/ruyi_device/output 'Continue'
         local next_step=(${result_item[@]})
         for step in ${next_step[@]}; do
-            recursion_run "$now_exec\n$step"
+            [ $step != 'n' ]
+            recursion_run "$step" $?
         done
         return 0;
     fi
     mv /tmp/ruyi_device/output /tmp/ruyi_device/output_${now_exec}
-    return 1;           
 
+    return 1;
 }
 
 function test_res() {
     file=$1
     res=0
-    grep 'Happy hacking!' $file
+
+    ret=$(grep 'Happy hacking!' $file)
     res=$(expr $res + $?)
+
+    ret_e=$(echo $ret | awk '{print 3}')
+    ret_g=$(echo $ret | awk '{print 4}')
+
+    ( [ $ret_e = 0 ] && [ $ret_g = 0 ] ) || ( [ $ret_e != 0 ] && [ $ret_g != 0 ] )
+    res=$(expr $res + $?)
+
     return $res
 }
-
-
-

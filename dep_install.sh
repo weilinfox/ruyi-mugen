d2196680947710e132d4ce24cceb3e5f46668184
weilinfox <caiweilin@iscas.ac.cn>
2024-03-19 14:10:08 +0800

    Add archlinux deps
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- dep_install.sh
+++ dep_install.sh
@@ -27,6 +27,7 @@ usage() {
 common_dep(){
     apt-get update && apt-get install -y expect psmisc iputils-ping make python3-paramiko python3-six || echo Not apt distro
     dnf install expect psmisc make iputils python3-six python3-paramiko -y || echo Not rpm distro
+    pacman --noconfirm -Syuu && pacman --need --noconfirm -S expect psmisc make iputils python-six python-paramiko || echo Not archlinux distro
 }
 
 qemu_dep(){
